ad6749e5fa5c9ff9500181ea3b8dec2f9d02eb10
crisp-bookmark-0117 <jostillo91@gmail.com>
2020-01-29 03:03:18 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,4 +1,4 @@
-https://learn.co/tracks/online-software-engineering-structured/procedural-ruby/variables-and-methods/methods-and-arguments-lab#
+
 def introduction_with_language(name, language)
   puts "Hi, my name is #{josh} and I am learning to program in #{ruby}."
 end
