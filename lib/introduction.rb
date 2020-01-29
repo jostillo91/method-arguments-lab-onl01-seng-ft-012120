47e5d38c296fba078832202752037ba10df41c51
crisp-bookmark-0117 <jostillo91@gmail.com>
2020-01-29 03:04:51 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,4 +1,6 @@
 
 def introduction_with_language(name, language)
-  puts "Hi, my name is #{josh} and I am learning to program in #{ruby}."
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
+
+introduction_with_language(josh, java)
